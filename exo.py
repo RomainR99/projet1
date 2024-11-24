6b99285ea18a2e46d737dad80a60879eb176fcfb
Romain <romain.roth@gmail.com>
2024-11-24 21:55:23 +0100

    créér un todo, lister les todos

--- exo.py
+++ exo.py
@@ -1,1 +1,17 @@
-xx
+def lister_todos():
+print('Fonctionnalité "lister les todos" à venir')
+# Fonction pour créer un todo - à réaliser
+def creer_todo():
+print('Fonctionnalité "créer un todo" à venir')
+# Menu principal
+choix = ''
+while choix != 'q':
+	# Affichage des choix
+	print('\n==== Menu principal ====')
+	print('1: Lister les todos')
+	print('2: Créer un todo')	
+	# Actions suivant le choix
+	choix = input('=> Choix : ')
+	match choix:
+		case '1': lister_todos()
+		case '2': creer_todo()
